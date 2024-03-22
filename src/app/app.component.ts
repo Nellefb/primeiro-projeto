7b5c38277736e41edc4900c93b5be51f741af9da
Nellefb <ellenfebar@gmail.com>
2024-03-21 22:15:30 -0300

    Exercicio de sala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { DecoradorInputComponent } from './components/decorador-input/decorador-input.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponentComponent, DecoradorInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
